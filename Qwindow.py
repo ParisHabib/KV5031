eac8076b057e1d889917da668cc5c3c0cbbce82f
Parisxhabib <Paris.habib@northumbria.ac.uk>
2026-02-06 23:57:43 +0000

    final

--- Qwindow.py
+++ Qwindow.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import QToolBar, QStatusBar, QMenuBar, QTextEdit, QInputDialog, QApplication, QListView, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QListWidget, QMessageBox, QLineEdit, QPushButton
 from PySide6.QtGui import QAction
-from licensee import Licensee
+from licensee import Licensee, Officer, Rehabilitation_Housing_Unit
 
 #Main Application Widget
 class MainWindow(QMainWindow):
@@ -14,6 +14,7 @@ class MainWindow(QMainWindow):
         menubar = self.menuBar() #create menus bar and add a file menu and licensee menu
         file_menu = menubar.addMenu("File")
         licensees_menu = menubar.addMenu("Licensees")
+        RHU_menu = menubar.addMenu("RHU")
         menubar.setNativeMenuBar(False) #found this on a forum because im on mac and menubar was appearing at the top and was confusing me
 
         add_action = QAction("Add", self) #add actions to file menu
@@ -24,11 +25,20 @@ class MainWindow(QMainWindow):
         delete_action.triggered.connect(self.delete_licensee)
         licensees_menu.addAction(delete_action)
 
+        add_RHU_action = QAction("Add RHU", self)
+        add_RHU_action.triggered.connect(self.add_RHU)
+        RHU_menu.addAction(add_RHU_action)
+
+        delete_RHU_action = QAction("Delete RHU", self)
+        delete_RHU_action.triggered.connect(self.delete_RHU)
+        RHU_menu.addAction(delete_RHU_action)
+
         exit_action = QAction("Exit", self)
         exit_action.triggered.connect(self.close)
         file_menu.addAction(exit_action)
 
         self.list_widget = QListWidget() #Create QListView
+        self.RHU_list_widget = QListWidget()
         #self.name_input = QLineEdit()
         self.add_button = QPushButton("Add")
         self.delete_button = QPushButton("Delete")
@@ -46,18 +56,28 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout() #Create vertical layout
         layout.addLayout(input_layout)
         layout.addWidget(self.list_widget) #Add the QListView to the layout
+        layout.addWidget(self.RHU_list_widget)
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
 
         self.licensees = [
-            Licensee("Test 1", "001"),
-            Licensee("Test 2", "002"),
-            Licensee("Test 3", "003")
+            Licensee("Test 1", "no notes", "no address", "male", "1st January", "1st february", "Strangeways", "001"),
+            Licensee("Test 2", "anger issues", "12 Albert Place", "female", "2nd January", "2nd February", "Strangeways", "002"),
+            Licensee("Test 3", "no notes", "37 Northumberland Street", "male", "3rd January", "3rd February", "Forest Bank", "003")
         ]
 
         for lic in self.licensees: #puts data into UI list
             self.list_widget.addItem(lic.get_name())
+
+        self.RHU = [
+            Rehabilitation_Housing_Unit("Unit 1", "no notes", "12 test street"),
+            Rehabilitation_Housing_Unit("Unit 2", "no males", "13 test street"),
+            Rehabilitation_Housing_Unit("Unit 3", "no notes", "14 test street")
+        ]
+
+        for unit in self.RHU:
+            self.RHU_list_widget.addItem(unit.get_RHU_name())
 
     def add_licensee(self):
         name, ok = QInputDialog.getText(
@@ -72,34 +92,77 @@ class MainWindow(QMainWindow):
                 "Enter ID"
             )
             if ok2 and personal_id != "":
-                licensee_select = Licensee(name, personal_id)
-                reply = QMessageBox.question(
+                notes, ok3 = QInputDialog.getText(
                     self,
-                    "Sex Offender",
-                    "Is he/she a sex offender?",
-                    QMessageBox.StandardButton.Yes |
-                    QMessageBox.StandardButton.No
+                    "Add Licensee",
+                    "Enter notes"
                 )
-                if reply == QMessageBox.StandardButton.Yes:
-                    licensee_select.set_sex_offender(True)
-                else:
-                    licensee_select.set_sex_offender(False)
-                
-                reply2 = QMessageBox.question(
-                    self,
-                    "Drug Search",
-                    "Does he/she require a drug search?",
-                    QMessageBox.StandardButton.Yes |
-                    QMessageBox.StandardButton.No
-                )
-                if reply2 == QMessageBox.StandardButton.Yes:
-                    licensee_select.set_needed_drug_search(True)
-                else:
-                    licensee_select.set_needed_drug_search(False)
-                self.licensees.append(licensee_select)
-                self.list_widget.addItem(licensee_select.get_name())
-                
-        
+                if ok3:
+                    home_address, ok4 = QInputDialog.getText(
+                        self, 
+                        "Add Licensee",
+                        "Enter home address"
+                    )
+                    if ok4:
+                        prisoner_gender, ok5 = QInputDialog.getText(
+                            self,
+                            "Add Licensee",
+                            "Enter gender"
+                        )
+                        if ok5:
+                            release_date, ok6 = QInputDialog.getText(
+                                self,
+                                "Add Licensee",
+                                "Enter release date"
+                            )
+                            if ok6:
+                                end_of_license, ok7 = QInputDialog.getText(
+                                    self,
+                                    "Add Licensee",
+                                    "Enter end of license date"
+                                )
+                                if ok7:
+                                    current_prison, ok8 = QInputDialog.getText(
+                                        self,
+                                        "Add Licensee",
+                                        "Enter current prison"
+                                    )
+                                    if ok8:
+                                        licensee_select = Licensee(
+                                            name,
+                                            notes,
+                                            home_address,
+                                            prisoner_gender,
+                                            release_date,
+                                            end_of_license,
+                                            current_prison,
+                                            personal_id
+                                        )
+                                        reply = QMessageBox.question(
+                                            self,
+                                            "Sex Offender",
+                                            "Is he/she a sex offender?",
+                                            QMessageBox.StandardButton.Yes |
+                                            QMessageBox.StandardButton.No
+                                        )
+                                        if reply == QMessageBox.StandardButton.Yes:
+                                            licensee_select.set_sex_offender(True)
+                                        else:
+                                            licensee_select.set_sex_offender(False)
+                                        
+                                        reply2 = QMessageBox.question(
+                                            self,
+                                            "Drug Search",
+                                            "Does he/she require a drug search?",
+                                            QMessageBox.StandardButton.Yes |
+                                            QMessageBox.StandardButton.No
+                                        )
+                                        if reply2 == QMessageBox.StandardButton.Yes:
+                                            licensee_select.set_needed_drug_search(True)
+                                        else:
+                                            licensee_select.set_needed_drug_search(False)
+                                        self.licensees.append(licensee_select)
+                                        self.list_widget.addItem(licensee_select.get_name())
 
     def delete_licensee(self):
         hitItems = self.list_widget.selectedItems()
@@ -114,9 +177,48 @@ class MainWindow(QMainWindow):
                 "Select licensee"
             )
 
+    def add_RHU(self):
+        name, ok = QInputDialog.getText(self, 
+                                        "Add RHU", 
+                                        "Enter RHU name")
+        if ok and name != "":
+            notes, ok2 = QInputDialog.getText(self,
+                                              "Add RHU",
+                                              "Enter notes")
+            if ok2:
+                address, ok3 = QInputDialog.getText(self,
+                                                    "Add RHU",
+                                                    "Enter address")
+                if ok3 and address != "":
+                    unit = Rehabilitation_Housing_Unit(name,
+                                                       notes,
+                                                       address)
+                    reply = QMessageBox.question(
+                        self,
+                        "Safe Distance from School",
+                        "Is this unit within 5 miles of a school?",
+                        QMessageBox.StandardButton.Yes |
+                        QMessageBox.StandardButton.No
+                    )
+                    if reply == QMessageBox.StandardButton.Yes:
+                        unit.set_within_school_distance(True)
+                    else:
+                        unit.set_within_school_distance(False)
+                    self.RHU.append(unit)
+                    self.RHU_list_widget.addItem(unit.get_RHU_name())
+
+    def delete_RHU(self):
+        hitItems = self.RHU_list_widget.selectedItems()
+        if hitItems is not None and len(hitItems) > 0:
+            row = self.RHU_list_widget.row(hitItems[0])
+            self.RHU_list_widget.takeItem(row)
+            self.RHU.pop(row)
+        else:
+            QMessageBox.information(self, "Delete RHU", "Select RHU")
+
     
     def on_item_double_clicked(self, item) -> None:
         row = self.list_widget.row(item)
         licensee_select = self.licensees[row]
         QMessageBox.information(self, "Selected", 
-                                f"Name: {licensee_select.get_name()} \nID: {licensee_select.get_id()} \nSex Offender: {licensee_select.get_sex_offender()} \nDrug Search Needed: {licensee_select.get_needed_drug_search()}")
+                                f"Name: {licensee_select.get_name()} \nID: {licensee_select.get_id()}, \nNotes: {licensee_select.get_notes()}, \nAddress: {licensee_select.get_home_address()} \nGender: {licensee_select.get_prisoner_gender()}, \nRelease: {licensee_select.get_release_date()}, \nEnd of License: {licensee_select.get_end_of_license()}, \nPrison: {licensee_select.get_current_prison()},  \nSex Offender: {licensee_select.get_sex_offender()} \nDrug Search Needed: {licensee_select.get_needed_drug_search()}")
--- licensee.py
+++ licensee.py
@@ -1,7 +1,8 @@
 class Person:
-    def __init__(self, name: str, personal_id: str = "000"):
+    def __init__(self, name: str, notes: str, personal_id: str = "000"):
         self.name = name
         self.personal_id = personal_id
+        self.notes = notes
 
     def get_name(self) -> str:
         return self.name
@@ -11,12 +12,26 @@ class Person:
 
     def get_id(self) -> str:
         return self.personal_id
+    
+    def set_id(self, personal_id: str) -> None:
+        self.personal_id = personal_id
+
+    def get_notes(self) -> str:
+        return self.notes
+    
+    def set_notes(self, notes: str) -> None:
+        self.notes = notes
 
 class Licensee(Person):
-    def __init__(self, name: str, personal_id: str = "000"):
-        super().__init__(name, personal_id)
+    def __init__(self, name: str, notes: str, home_address: str, prisoner_gender: str, release_date: str, end_of_license: str, current_prison: str, personal_id: str = "000"):
+        super().__init__(name, notes, personal_id)
         self.sex_offender = False
         self.needed_drug_search = False
+        self.home_address = home_address
+        self.prisoner_gender = prisoner_gender
+        self.release_date = release_date
+        self.end_of_license = end_of_license
+        self.current_prison = current_prison
 
     def get_sex_offender(self) -> bool:
         return self.sex_offender
@@ -30,32 +45,68 @@ class Licensee(Person):
     def set_needed_drug_search(self, status: bool) -> None:
         self.needed_drug_search = status
 
+    def get_home_address(self) -> str:
+        return self.home_address
+    
+    def set_home_address(self, home_address: str) -> None:
+        self.home_address = home_address
+
+    def get_prisoner_gender(self) -> str:
+        return self.prisoner_gender
+    
+    def set_prisoner_gender(self, prisoner_gender: str) -> None:
+        self.prisoner_gender = prisoner_gender
+
+    def get_release_date(self) -> str:
+        return self.release_date
+    
+    def set_release_date(self, release_date: str) -> None:
+        self.release_date = release_date
+
+    def get_end_of_license(self) -> str:
+        return self.end_of_license
+    
+    def set_end_of_license(self, end_of_license: str) -> None:
+        self.end_of_license = end_of_license
+
+    def get_current_prison(self) -> str:
+        return self.current_prison
+    
+    def set_current_prison(self, current_prison: str) -> None:
+        self.current_prison = current_prison
+
 class Officer(Person):
-    def __init__(self, name: str, personal_id: str ="000"):
-        super().__init__(name, personal_id)
+    def __init__(self, name: str, notes: str, personal_id: str ="000"):
+        super().__init__(name, notes, personal_id)
 
-#class Licensee:
-    #def __init__(self, name: str, licensee_id: str) -> None:
-        #self.__name = name
-        #self.__licensee_id = licensee_id
-        #self.__sex_offender = False
+class Rehabilitation_Housing_Unit:
+    def __init__(self, RHU_name: str, RHU_notes: str, unit_address: str):
+        self.RHU_name = RHU_name
+        self.RHU_notes = RHU_notes
+        self.unit_address = unit_address
+        self.within_school_distance = False
 
-    #def get_name(self) -> str:
-        #return self.__name
+    def get_RHU_name(self) -> str:
+        return self.RHU_name
     
-    #def set_name(self, name: str) -> None:
-        #self.__name = name
-    
-    #def get_licensee_id(self) -> str:
-        #return self.__licensee_id
-    
-    #def set_licensee_id(self, licensee_id: str) -> None:
-        #self.__licensee_id = licensee_id
+    def set_RHU_name(self, RHU_name: str) -> None:
+        self.RHU_name = RHU_name
 
-    #def get_sex_offender(self) -> bool:
-        #return self.__sex_offender
+    def get_RHU_notes(self) -> str:
+        return self.RHU_notes
     
-    #def set_sex_offender(self, status: bool) -> None:
-        #self.__sex_offender = status
+    def set_RHU_notes(self, RHU_notes: str) -> None:
+        self.RHU_notes = RHU_notes
 
+    def get_unit_address(self) -> str:
+        return self.unit_address
+    
+    def set_unit_address(self, unit_address: str) -> None:
+        self.unit_address = unit_address
+   
+    def get_within_school_distance(self) -> bool:
+        return self.within_school_distance
+    
+    def set_within_school_distance(self, status: bool) -> None:
+        self.within_school_distance = status
 
